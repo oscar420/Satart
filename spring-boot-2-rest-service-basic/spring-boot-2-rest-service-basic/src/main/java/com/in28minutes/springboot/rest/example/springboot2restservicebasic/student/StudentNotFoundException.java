9356a1740f30137cad8cf147ea8ddf7f93dba6ac
oscar420 <66745768+oscar420@users.noreply.github.com>
2021-05-14 14:49:15 -0600

    Add files via upload

---
+++ spring-boot-2-rest-service-basic/spring-boot-2-rest-service-basic/src/main/java/com/in28minutes/springboot/rest/example/springboot2restservicebasic/student/StudentNotFoundException.java
@@ -0,0 +1,9 @@
+package com.in28minutes.springboot.rest.example.springboot2restservicebasic.student;
+
+public class StudentNotFoundException extends RuntimeException {
+
+	public StudentNotFoundException(String exception) {
+		super(exception);
+	}
+
+}
---
+++ spring-boot-2-rest-service-basic/spring-boot-2-rest-service-basic/src/test/java/com/in28minutes/springboot/rest/example/springboot2restservicebasic/SpringBoot2RestServiceBasicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.in28minutes.springboot.rest.example.springboot2restservicebasic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBoot2RestServiceBasicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
